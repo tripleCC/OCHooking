8b3ee191fac9f8befb354021a252872baf102701
tripleCC <triplec.linux@gmail.com>
2019-04-19 11:01:57 +0800

    feat: complete _Nonnull

--- OCHooking/Classes/OCHooking.h
+++ OCHooking/Classes/OCHooking.h
@@ -14,10 +14,10 @@ NS_ASSUME_NONNULL_BEGIN
 + (SEL)swizzledSelectorForSelector:(SEL)selector;
 
 + (void)swizzleMethod:(SEL)originalSelector onClass:(_Nonnull Class)cls withSwizzledSelector:(SEL)swizzledSelector;
-+ (void)swizzleClassMethod:(SEL)originalSelector onClass:(Class)cls withSwizzledSelector:(SEL)swizzledSelector;
++ (void)swizzleClassMethod:(SEL)originalSelector onClass:(_Nonnull Class)cls withSwizzledSelector:(SEL)swizzledSelector;
 
 + (NSInvocation *)swizzleMethod:(SEL)originalSelector onClass:(_Nonnull Class)cls withBlockMethod:(OCHBlockMethod * _Nonnull)blockMethod;
-+ (NSInvocation *)swizzleClassMethod:(SEL)originalSelector onClass:(Class)cls withBlockMethod:(OCHBlockMethod *)blockMethod;
++ (NSInvocation *)swizzleClassMethod:(SEL)originalSelector onClass:(_Nonnull Class)cls withBlockMethod:(OCHBlockMethod *)blockMethod;
 @end
 
 NS_ASSUME_NONNULL_END
